05310113417002ea799ba4f454ecdf939c27b693
Bob Egan <bob.egan.dev@gmail.com>
2024-07-06 11:51:57 -0400

    Update scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,6 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
     ],
-    ignore['**/src/**', '**/node_modules/**'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,26 +1,31 @@
 import axios from 'axios'
+import data from './doclist.ts'
 
 axios.defaults.baseURL = 'http://localhost:8080/api';
 
 export default {
     getUsername: async ()=>{
-      const res = await axios.get('/getusername')
-        return res.data.username
+      //const res = await axios.get('/getusername')
+        //return res.data.username
+        return 'jsmith'
     },
     healthCheck: async ()=>{
-      const res = await axios.get('/')
-        console.log(res)
-      return res.data;
+      return "I'm alive";
+      //   const res = await axios.get('/')
+      //   console.log(res)
+      // return res.data;
     },
     getMedicalProfessionals : async () => {
-        const result = await axios.get(`/doclist`)
-        console.log(result.data)
-        return result.data;
+        return data;
+        // const result = await axios.get(`/doclist`)
+        // console.log(result.data)
+        // return result.data;
     },
     getProfessional: async (npi:string) =>{
-        const result = await axios.get(`/doclist?npi=${npi}`)
-        console.log('client api:', result)
-        localStorage.setItem(npi, JSON.stringify(result))
-        return result;
+        return data.filter(x=>x.NPI === npi)[0]
+        // const result = await axios.get(`/doclist?npi=${npi}`)
+        // console.log('client api:', result)
+        // localStorage.setItem(npi, JSON.stringify(result))
+        // return result;
     }
 }
