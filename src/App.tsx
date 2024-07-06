3f8465fb76ac229ed3075ff190e3f2383d6308df
Bob Egan <bob.egan.dev@gmail.com>
2024-07-06 12:51:50 -0400

    almost there....

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,9 @@ function App() {
             <Site>
                 <BrowserRouter>
                     <Routes>
-                        <Route path={'/providers'} element={<Providers/>} />
-                        <Route path={'/search'} element={<Providers/>} />
+                        <Route path={'/rantikore/providers'} element={<Providers/>} />
+                        <Route path={'/rantikore/search'} element={<Providers/>} />
+                        <Route path={'/rantikore'} element={<Home/>} />
                         <Route path={'/'} element={<Home/>} />
                     </Routes>
                 </BrowserRouter>
